3d680d64c43b3a871ac2d11a97993092853494f7
29834329834932840320392 <rmalanowski3@gmail.com>
2026-06-23 05:56:42 -0500

    Add forecast calculation helpers

---
+++ src/lib/types.ts
@@ -0,0 +1,26 @@
+export type Period = 'Month' | 'Week'
+
+export type Channel = {
+  id: string
+  name: string
+  color: string
+  leads: number
+  apptSetPct: number
+  showPct: number
+  closePct: number
+}
+
+export type PlannerState = {
+  newGoal: number
+  usedGoal: number
+  avgGross: number
+  period: Period
+  channels: Channel[]
+}
+
+export type SavedScenario = {
+  id: string
+  name: string
+  updatedAt: string
+  state: PlannerState
+}
